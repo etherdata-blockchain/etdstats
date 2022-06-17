42afd69e222d569260e383e16f2b10c2b50afb3f
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-06-17 15:43:32 +0800

    feat: add env checker (#16)
    * feat: add env checker
    
    * feat: rename packages

--- services/transaction_service/Package.swift
+++ services/transaction_service/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "transaction_service",
     platforms: [
-       .macOS(.v12)
+        .macOS(.v12),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
@@ -12,9 +12,9 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-mongo-driver.git", from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        .package(path: "../../packages/common"),
-        .package(path: "../../packages/model")
-        
+        .package(path: "../../swift_packages/common"),
+        .package(path: "../../swift_packages/model"),
+        .package(path: "../../swift_packages/env"),
     ],
     targets: [
         .target(
@@ -25,16 +25,17 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Alamofire", package: "Alamofire"),
                 .product(name: "common", package: "common"),
-                .product(name: "model", package: "model")
+                .product(name: "model", package: "model"),
+                .product(name: "env", package: "env"),
             ],
             swiftSettings: [
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
             ]
         ),
         .executableTarget(name: "Run", dependencies: [.target(name: "App")]),
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
-        ])
+        ]),
     ]
 )
