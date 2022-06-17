e3ba9c415457e7cbcbfb35620afd7bc33a2b705f
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-06-17 22:29:51 +0800

    feat: update openapi spec (#17)

--- node_packages/openapi_specs/src/index.ts
+++ node_packages/openapi_specs/src/index.ts
@@ -1,4 +1,8 @@
-import health_service_schema from "./health_service.spec.json";
-import transaction_service_schema from "./transaction_service.spec.json";
+import health_service_schema from "./health_service.spec.json"
+import transaction_service_schema from "./transaction_service.spec.json"
 
-export { health_service_schema, transaction_service_schema };
+
+export {
+  health_service_schema,
+  transaction_service_schema,  
+}
