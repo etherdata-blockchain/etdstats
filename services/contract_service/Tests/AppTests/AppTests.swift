35699368fc66bfde2bedf95c38bb9003e5cef7f2
sirily11 <sirily1997@gmail.com>
2022-11-12 22:36:24 +0800

    fix: issue

--- services/contract_service/Tests/AppTests/AppTests.swift
+++ services/contract_service/Tests/AppTests/AppTests.swift
@@ -15,20 +15,23 @@ import model
 final class AppTests: XCTestCase {
     
     var db: MongoDatabase?
-
+    
     override func setUpWithError() throws {
-       setenv(ENVIRONMENT_DB_KEY, "mongodb://root:password@localhost:27017/test", 1)
-       db = try MongoDatabase.synchronousConnect("mongodb://root:password@localhost:27017/test")
+        setenv(ENVIRONMENT_DB_KEY, "mongodb://root:password@localhost:27017/test", 1)
+        setenv(ENVIRONMENT_JWT_KEY, "password", 1)
+        setenv(ENVIRONMENT_SOLIDITY_SERVICE_URL_KEY, "https://solidity_service_url.com", 1)
+        
+        db = try MongoDatabase.synchronousConnect("mongodb://root:password@localhost:27017/test")
         
         let contract = model.ContractCreateDto(blockNumber: "0x0", blockTime: 0, creator: "0xabcde", transactionHash: "0xabcde", blockHash: "0xabcde", byteCode: "", address: "0xabcde", lastScannedBlock: 0)
         let contracts = db?[model.Contract.schema]
         let _ = try contracts?.insertEncoded(contract).wait()
     }
-
+    
     override func tearDownWithError() throws {
         try db?.drop().wait()
     }
-
+    
     func testCRUDContracts() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
